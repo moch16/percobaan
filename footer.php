d832815a2565d080f7eb840c28a1084ba87240d7
moch16 <mochamad.isep16@gmail.com>
2017-01-04 05:36:20 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,102 @@
+<!-- FOOTER -->
+<footer>
+  <div class="container">
+    <div class="row footer-widgets">
+      <!-- ARTIKEL TERBARU -->
+      <div class="col-md-4 col-sm-12 col-xs-12">
+        <div class="footer-widget recent-posts-widget">
+          <h4>ARTIKEL POPULER<span class="head-line"></span></h4>
+          <ul class="posts-list">
+            <!-- 
+              (ISI CODING PHP-nya DISINI) 
+            -->
+            <li>
+              <div class="widget-content">
+                <span><i class="fa fa-angle-right"></i>
+                  <a href="">Lorem ipsum 1</a>
+                </span>
+              </div>
+              <div class="clearfix"></div>
+            </li>
+            <li>
+              <div class="widget-content">
+                <span><i class="fa fa-angle-right"></i>
+                  <a href="">Lorem ipsum 2</a>
+                </span>
+              </div>
+              <div class="clearfix"></div>
+            </li>
+            <!-- 
+              (ISI CODING PHP-nya DISINI) 
+            -->
+          </ul>
+        </div>
+      </div>
+      <!-- ARTIKEL TERBARU END -->
+
+      <!-- LABEL ARTIKEL -->
+      <div class="col-md-4 col-sm-12 col-xs-12">
+        <div class="footer-widget tags-widget">
+          <div class="tags">
+            <h4>LABEL ARTIKEL<span class="head-line"></span></h4>
+            <ul class="tag" style="margin-bottom:0px;">
+            <!-- 
+              (ISI CODING PHP-nya DISINI) 
+            -->
+            <li>
+              <a href=''>
+                <i class='fa fa-tags pr-5'></i> &nbsp;
+                label 1
+              </a>
+            </li>
+            <li>
+              <a href=''>
+                <i class='fa fa-tags pr-5'></i> &nbsp;
+                label 2
+              </a>
+            </li>
+              <!-- 
+              (ISI CODING PHP-nya DISINI) 
+            -->
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- LABEL ARTIKEL END -->
+
+    <!-- HUBUNGI KAMI -->
+    <div class="col-md-4 col-sm-12 col-xs-12">
+      <div class="footer-widget recent-posts-widget">
+        <h4>HUBUNGI KAMI<span class="head-line"></span></h4>
+        <div class="contact-details">
+          <!-- 
+              (ISI CODING PHP-nya DISINI) 
+            -->
+            <p><i class="fa fa-phone fa-fw"></i>&nbsp;Telepon :&nbsp;000-000-000</p>
+            <p><i class="fa fa-envelope fa-fw"></i>&nbsp;E-mail :&nbsp;contoh@ummi.com</p>
+            <!-- Divider -->
+            <div class="hr2" style="margin-top:5px; margin-bottom:5px;"></div>
+            <p>isi alamat</p>
+          </div>
+        </div>
+      </div>
+      <!-- HUBUNGI KAMI END -->
+
+    </div>
+  </div>
+</footer>
+<!-- FOOTER END -->
+
+<!-- HAK CIPTA -->
+<div class="copyright-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <p>Copyright &copy; 2016 - Pelatihan Membuat Website Mahasiswa Teknik Informatika</p>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
+</div>
+<!-- HAK CIPTA -->
